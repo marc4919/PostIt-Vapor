0c63038cb4d117c54f7353fdf7d57d3c7f87f31c
marc4919 <marcgeek4919@gmail.com>
2025-05-18 21:34:14 +0200

    chore: add local database fallback configuration

--- Sources/PostIt/configure.swift
+++ Sources/PostIt/configure.swift
@@ -6,7 +6,7 @@ import Vapor
 
 public func configure(_ app: Application) async throws {
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
+    
     if let databaseURL = Environment.get("DATABASE_URL") {
         try app.databases.use(.postgres(url: databaseURL), as: .psql)
         app.logger.info("Connected")
@@ -18,6 +18,20 @@ public func configure(_ app: Application) async throws {
     app.migrations.add(PostsMigration())
 
     try await app.autoMigrate()
+
+    // LOCAL
+    /*
+    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+        username: Environment.get("DATABASE_USERNAME") ?? "vapor_user",
+        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+        database: Environment.get("DATABASE_NAME") ?? "vapor-database",
+        tls: .prefer(try .init(configuration: .clientDefault)))
+    ), as: .psql)
+
+    app.migrations.add(PostsMigration())
+    */
 
     app.views.use(.leaf)
     app.leaf.configuration.rootDirectory = "Resources/Views"
