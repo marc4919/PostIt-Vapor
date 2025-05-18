60095391cfcb5ae1b916010dd0a0d7565ce83776
marc4919 <marcgeek4919@gmail.com>
2025-05-18 20:44:21 +0200

    feat: add redirect to posts routes

--- Sources/PostIt/routes.swift
+++ Sources/PostIt/routes.swift
@@ -2,5 +2,9 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
+    app.get { req in
+            req.redirect(to: "/posts")
+    }
+    
     try app.routes.register(collection: PostController())
 }
